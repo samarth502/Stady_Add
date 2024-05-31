e46cd09898725ee96295fb5a9277150f60b7610a
alphacalling <vikaspandey490@gmail.com>
2024-05-31 11:21:43 +0530

    spell checker

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -109,9 +109,9 @@ const About = () => {
       </section>
 
       <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
-        {/* Reviws from Other Learner */}
+        {/* Reviews from Other Learner */}
         <h1 className="text-center text-4xl font-semibold mt-8">
-          Reviews from other learners
+          Reviews from other Learners
         </h1>
         <ReviewSlider />
       </div>
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -22,7 +22,7 @@ const Contact = () => {
       <div className="relative mx-auto my-20 flex w-11/12 max-w-maxContent flex-col items-center justify-between gap-8 bg-richblack-900 text-white">
         {/* Reviews from Other Learner */}
         <h1 className="text-center text-4xl font-semibold mt-8">
-          Reviews from other learners
+          Reviews from other Learners
         </h1>
         <ReviewSlider />
       </div>
